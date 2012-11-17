77002c07c8fdda2e2a02472b32dbb388bb4dddcf
Jacob Vorreuter <jacob.vorreuter@gmail.com>
2012-11-16 22:05:21 -0800

    load cards into cells

--- web.rb
+++ web.rb
@@ -3,6 +3,7 @@ require 'redis'
 require 'oa-openid'
 require 'openid_redis_store'
 require 'trello'
+require 'json'
 
 include Trello
 include Trello::Authorization
@@ -12,6 +13,20 @@ $stdout.sync = true
 Trello::Authorization.const_set :AuthPolicy, OAuthPolicy
 OAuthPolicy.consumer_credential = OAuthCredential.new ENV['TRELLO_API_KEY'], ENV['TRELLO_OAUTH_SECRET']
 OAuthPolicy.token = OAuthCredential.new ENV['TRELLO_USER_KEY'], nil
+
+class Card < BasicData
+  def cell
+    self.actions.select {|a|
+      a.type == "commentCard" 
+    }.map {|c|
+      c.data["text"].match('(impact|effort)=(high|medium|low),\s?(impact|effort)=(high|medium|low)')
+    }.select {|m|
+      !m.nil? && m.length == 5
+    }.map {|m|
+      {m[1].to_sym => m[2], m[3].to_sym => m[4]}
+    }.first rescue nil
+  end
+end
 
 class MyApp < Sinatra::Application
 
@@ -32,13 +47,51 @@ class MyApp < Sinatra::Application
   end
 
   get '/boards/:id' do
-    check_session
+    #check_session
     @board = Board.find(params[:id])
-    @cards = @board.cards
+    cards = @board.cards.map {|c| [c.cell, c] }
+    @buckets = divide_cards(cards)
     erb :board
   end
 
 protected
+  def divide_cards(cards)
+    buckets = initialize_buckets
+    # [impact, effort]
+    layout = [["high",   "high"], ["high",   "medium"], ["high",   "low"],
+              ["medium", "high"], ["medium", "medium"], ["medium", "low"],
+              ["low",    "high"], ["low",    "medium"], ["low",    "low"]]
+    layout.each_with_index do |pair,index|
+      impact, effort = pair
+      cards.each do |cell, card|
+        if cell then
+          if cell[:impact] == impact && cell[:effort] == effort then
+            buckets[(index+1).to_s.to_sym] << card
+          end
+        end
+      end
+    end
+    cards.each do |cell, card|
+      unless cell then
+        buckets[:'0'] << card
+      end
+    end
+    return buckets
+  end
+
+  def initialize_buckets
+    {:'1' => [],
+     :'2' => [],
+     :'3' => [],
+     :'4' => [],
+     :'5' => [],
+     :'6' => [],
+     :'7' => [],
+     :'8' => [],
+     :'9' => [],
+     :'0' => []}
+  end
+
   def check_session
     if !session["authorized"]
       session["from"] = "/"
